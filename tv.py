ac877833d23cf6dbae4a310dd13e64115c6f953d
Kyle Delaney <kyle.reid.delaney@gmail.com>
2021-04-30 23:07:08 -0400

    fixed off-by-one fastforwarding, added daily schedules

--- tv.py
+++ tv.py
@@ -8,8 +8,10 @@ import argparse
 import time
 import shlex
 import csv
+import calendar
 
 allowed_extensions = ('.mp4','.mkv','.avi')
+seed_bits = 32
 
 def in_dir(prefix,f):
     return os.path.dirname(f) == prefix
@@ -26,7 +28,9 @@ class MetaPool:
         self._index = (self._index + 1) % len(self._children)
 
     def shuffle(self):
-        random.shuffle(self._children)
+        #don't want the state of random to depend on len()
+        r = random.Random(random.getrandbits(seed_bits))
+        r.shuffle(self._children)
         self._index = 0
         for child in self._children:
             child.shuffle()
@@ -45,7 +49,8 @@ class Pool:
         pass
 
     def shuffle(self):
-        random.shuffle(self._videos)
+        r = random.Random(random.getrandbits(seed_bits))
+        r.shuffle(self._videos)
         self._index = 0
 
     def grab(self):
@@ -56,7 +61,8 @@ class Pool:
                 return item
 
 class Program:
-    def __init__(self, file):
+    def __init__(self, file, pools):
+        self._pools = pools
         defs = {}
         params = {
             'seed_mutator':':0001',
@@ -86,54 +92,88 @@ class Program:
                         params['start_hour'] = int(tokens[1])
                     else:
                         params[tokens[0]] = tokens[1]
+        weekdays = [
+            '__monday__',
+            '__tuesday__',
+            '__wednesday__',
+            '__thursday__',
+            '__friday__'
+        ]
+        weekends = [
+            '__saturday__',
+            '__sunday__'
+        ]
+        for day in weekdays:
+            if day not in defs:
+                defs[day] = defs['__weekday__'] if '__weekday__' in defs else defs['__every_day__']
+        for day in weekends:
+            if day not in defs:
+                defs[day] = defs['__weekend__'] if '__weekend__' in defs else defs['__every_day__']
+
         self.defs = defs
         self.params = params
 
-class ProgramState:
-    def __init__(self):
-        self.current_pool = None
-        self.pool_stack = []
+    def run(self,entrypoint=None,wday=None):
+        pools = self._pools
 
-def build_playlist(program,pools,entrypoint='__every_day__'):
-    def primitive(tokens,playlist,state):
-        command, *args = tokens
-        if command == 'using':
-            state.current_pool = args[0]
-        elif command == 'play':
-            for i in range(int(args[0])):
-                playlist.append(pools[state.current_pool].grab())
-        elif command == 'shuffle':
-            pools[state.current_pool].shuffle()
-        elif command == 'pick':
-            pools[state.current_pool].pick()
-        elif command == 'file':
-            playlist.append(os.path.join(videos_path,args[0]))
-        elif command == 'oneof':
-            primitive([random.choice(args)],playlist,state)
-        else:
-            repeat = 1
-            if len(tokens) > 1:
-                repeat = int(tokens[1])
-            for i in range(repeat):
-                state.pool_stack.append(state.current_pool)
-                for tokens in program.defs[command]:
-                    primitive(tokens,playlist,state)
-                state.current_pool = state.pool_stack.pop()
-    playlist = []
-    state = ProgramState()
-    for item in program.defs[entrypoint]:
-        primitive(item,playlist,state)
-    return playlist
+        class ProgramState:
+            def __init__(self):
+                self.current_pool = None
+                self.pool_stack = []
+
+        def primitive(tokens,playlist,state):
+            command, *args = tokens
+            if command == 'using':
+                state.current_pool = args[0]
+            elif command == 'play':
+                for i in range(int(args[0])):
+                    playlist.append(pools[state.current_pool].grab())
+            elif command == 'shuffle':
+                pools[state.current_pool].shuffle()
+            elif command == 'pick':
+                pools[state.current_pool].pick()
+            elif command == 'file':
+                playlist.append(os.path.join(videos_path,args[0]))
+            elif command == 'oneof':
+                primitive([random.choice(args)],playlist,state)
+            else:
+                repeat = 1
+                if len(tokens) > 1:
+                    repeat = int(tokens[1])
+                for i in range(repeat):
+                    state.pool_stack.append(state.current_pool)
+                    for tokens in self.defs[command]:
+                        primitive(tokens,playlist,state)
+                    state.current_pool = state.pool_stack.pop()
+
+        wdays = [
+            '__monday__',
+            '__tuesday__',
+            '__wednesday__',
+            '__thursday__',
+            '__friday__',
+            '__saturday__',
+            '__sunday__'
+        ]
+
+        if entrypoint is None:
+            entrypoint = wdays[wday]
+
+        playlist = []
+        state = ProgramState()
+        for item in self.defs[entrypoint]:
+            primitive(item,playlist,state)
+        return playlist
+
 
 def play(playlist,marquee,start_time=None):
     args = [
         'cvlc',
         '--play-and-exit',
         '--no-video-title-show',
-        '--audio-filter', 'normvol',
-        '--norm-max-level','10',
         '--audio-filter', 'compressor',
         '--sub-source='+marquee,
+        '--image-duration=60'
     ]
     if start_time is not None:
         args += ['--start-time',str(start_time)]
@@ -144,7 +184,8 @@ def off_air(playlist):
         'cvlc',
         '--play-and-exit',
         '--no-video-title-show',
-        '--sub-source=marq{marquee=%I:%M%p,size=32,color=0xffffff,position=8,x=20,y=20}'
+        '--audio-filter', 'compressor',
+        '--sub-source=marq{marquee=%I:%M%p,size=32,color=0xffffff,position=8,x=20,y=20}',
         '--image-duration=60'
     ]+playlist)
 
@@ -155,6 +196,7 @@ if __name__ == '__main__':
     parser.add_argument('--times','-t', default='./Videos/times.csv')
     parser.add_argument('--dry-run','-N', action='store_true')
     parser.add_argument('--no-wait', action='store_true')
+    parser.add_argument('--date')
     args = parser.parse_args()
 
     videos_path = args.sources
@@ -164,44 +206,52 @@ if __name__ == '__main__':
     pools = {}
     pool_keys = None
 
-    with open(args.times,newline='') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            times_db[os.path.join(videos_path,row['file'])] = int(row['duration'])
-
-    with open(program_path) as file:
-        program = Program(file)
-
-    random.seed(program.params['seed'],2)
-    start_day = program.params['start_day']
-    start_hour = program.params['start_hour']
-    marquee = program.params['marquee']
-
     for root, dirs, files in os.walk(videos_path):
         if root == videos_path:
             pool_keys = dirs
         for file in files:
             if file.endswith(allowed_extensions):
                 videos.append(os.path.join(root,file))
-
     for k in pool_keys:
         pools[k] = MetaPool(list(filter(lambda f: '/{}/'.format(k) in f, videos)),k)
 
+    with open(args.times,newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            times_db[os.path.join(videos_path,row['file'])] = int(row['duration'])
 
-    build_playlist(program,pools,'__init__')
-    off_air_playlist = build_playlist(program,pools,'__off_air__')
+    with open(program_path) as file:
+        program = Program(file,pools)
+
+    random.seed(program.params['seed'],2)
+    start_day = program.params['start_day']
+    start_hour = program.params['start_hour']
+    marquee = program.params['marquee']
+
+
+
+    program.run('__init__')
+    off_air_playlist = program.run('__off_air__')
 
     #wait for NTP sync
     if not args.no_wait:
         off_air(off_air_playlist)
 
     #fastforward on first boot
-    days = int(time.time() - time.mktime(time.strptime(start_day,'%Y-%m-%d'))) // (60*60*24)
-    print(days)
-    for i in range(days):
-        playlist = build_playlist(program,pools)
+    start_tm = time.strptime(start_day,'%Y-%m-%d')
+    days = int(
+        (time.mktime(time.strptime(args.date,'%Y-%m-%d')) if args.date else time.time())
+        - time.mktime(start_tm)
+    )//(60*60*24)
+    print('fastforwarded {} days'.format(days))
+    for i in range(days+1):
+        playlist = program.run(wday=(start_tm.tm_wday + i)%7)
     if args.dry_run:
         total_time = 0
+        print('start weekday {}, end weekday {}'.format(
+            calendar.day_name[start_tm.tm_wday],
+            calendar.day_name[(start_tm.tm_wday + days)%7]
+        ))
         for item in playlist:
             print('{:>02.0f}:{:>02.0f} {:>74}'.format(total_time/(60*60) + start_hour,(total_time/60)%60,item[-74:]))
             total_time += times_db[item]
@@ -239,4 +289,4 @@ if __name__ == '__main__':
         bg_index = 0
         while time.localtime().tm_hour != start_hour:
             off_air(off_air_playlist)
-        play(build_playlist(program,pools),marquee)
+        play(program.run(wday=time.localtime().tm_wday),marquee)
